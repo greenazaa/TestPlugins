f68d27afc2fd0951ce347044c3f9da6ef477e950
greenazaa <greenazaa@gmail.com>
2025-05-27 00:37:13 +0530

    Added build.gradle.kts

---
+++ DoraBashProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("cloudstream-plugin")
+}
+
+cloudstream {
+    language = "hi"
+    description = "Streams Hindi movies and shows from DoraBash"
+    authors = listOf("greenazaa")
+
+    status = 1 // 0 = Broken, 1 = Working, 2 = Partially working
+    tvTypes = listOf("Movie", "TvSeries")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=dorabash.com&sz=128"
+}
