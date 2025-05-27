4c581e466e8f027d727426d5b2554871b95d564c
greenazaa <greenazaa@gmail.com>
2025-05-27 09:09:41 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included. Every time you add a new project, you must add it
-// to the includes below.
-
-// Plugins are included like this
 include("DoraBashProvider")
